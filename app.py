a0cff76807be8f97714d4b18741967858936808c
Chirra Nakshatra <2303a51094@sru.edu.in>
2026-06-16 11:25:48 +0530

    Update project files

--- app.py
+++ app.py
@@ -308,6 +308,4 @@ def change_language():
 
  
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(debug=False, host="0.0.0.0", port=port)
     app.run(debug=False, host='localhost', port=5000)
